68315fc44059df3e70d3a87af000f4b6436e37e0
Brendon Ho <brendonho01@users.noreply.github.com>
2015-07-10 09:33:58 -0400

    Update gamefunctions.js

--- App/gamefunctions.js
+++ App/gamefunctions.js
@@ -1,5 +1,15 @@
 let creator = "Brendon Ho";
 let developer = "Brendon Ho";
 
+//player functions
 var gravity = 0.5;
-var 
+var fallSpeed = 9;
+var positionX = 200;
+var positionY = 0;
+var charSize = 18;
+
+//game functions
+var platform = function(x,y,width,height){
+
+};
+
