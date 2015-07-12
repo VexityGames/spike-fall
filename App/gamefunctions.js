515ec4453009fcfb06cb6885b347b56dcc1eb94f
Brendon Ho <brendonho01@users.noreply.github.com>
2015-07-11 22:35:15 -0400

    Update gamefunctions.js

--- App/gamefunctions.js
+++ App/gamefunctions.js
@@ -1,6 +1,6 @@
 let creator = "Brendon Ho";
-let developers = "Brendon Ho","","";
-let helpers = "";
+let developers = ["Brendon Ho","","";]
+let helpers = ["";]
 //player functions
 var gravity = 0.5;
 var fallSpeed = 9;
@@ -8,6 +8,16 @@ var positionX = 200;
 var positionY = 0;
 var charSize = 18;
 
+//Wall Coordinates
+var wallOneX = /*Add Value*/;
+var wallOneY = /*Add Value*/;
+var wallTwoX = /*Add Value*/;
+var wallTwoY = /*Add Value*/;
+var wallOneWidth = /*Add Value*/;
+var wallOneHeight = /*Add Value*/;
+var wallOneHeight = /*Add Value*/;
+var wallTwoWidth = /*Add Value*/;
+var wallTwoHeight = /*Add Value*/;
 
 //game functions
 var walls = [];
@@ -20,8 +30,8 @@ var makeWalls = function(x,y,width,height){
 };
 makeWalls();
 var drawWalls = function(){
-  for(var each in platforms) {
-        rect(platforms[each].x, platforms[each].y, platforms[each].width, platforms[each].height);
+  for(var each in walls) {
+        rect(walls[each].x, walls[each].y, walls[each].width, walls[each].height);
   }
 };
 
@@ -29,8 +39,9 @@ var spikes = [];
 var makeSpikes = function(x,y,width,height){
   
 };
+
 var movePlayer = function(){
   if(mousePressed){};
-  if(x = 15){};
+  if(positionX = 15){};
 
 
