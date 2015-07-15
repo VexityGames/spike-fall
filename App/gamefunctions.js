4cc52c3c6db51298d3b7311d90fc2771fce213b4
Brendon Ho <brendonho01@users.noreply.github.com>
2015-07-15 17:58:43 -0400

    Update gamefunctions.js

--- App/gamefunctions.js
+++ App/gamefunctions.js
@@ -1,6 +1,6 @@
 let creator = "Brendon Ho";
-let developers = ["Brendon Ho","","";]
-let helpers = ["";]
+let developers = ["Brendon Ho","Peter Soboyejo","";]
+let helpers = ["Rahul Nadakumar";]
 //player functions
 var gravity = 0.5;
 var fallSpeed = 9;
